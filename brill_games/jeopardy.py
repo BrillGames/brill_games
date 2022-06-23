e3d33a3db4f0d4b75aea4899d2a0383b6856bd7e
chrisphamm521 <chris.pham2003@rocketmail.com>
2022-06-22 21:34:28 -0400

    Added Board class to jeopardy.py with constructor, getters , and setters

--- brill_games/jeopardy.py
+++ brill_games/jeopardy.py
@@ -18,11 +18,37 @@ class Answer:
     def setAnswer(self, x):
         self.__answer = x
 
-    def setQuestion(self, y):
-        self.__question = y
+    def setQuestion(self, x):
+        self.__question = x
 
-    def setPoints(self, z):
-        self.__points = z
+    def setPoints(self, x):
+        self.__points = x
+
+
+class Board:
+    def __init__(self, answers, columns, players):
+        self.__answers = answers
+        self.__columns = columns
+        self.__players = players
+
+    def getAnswers(self):
+        return self.__answers
+
+    def getColumns(self):
+        return self.__columns
+
+    def getPlayers(self):
+        return self.__players
+
+    def setAnswers(self, x):
+        self.__answers = x
+
+    def setColumns(self, x):
+        self.__columns = x
+
+    def setPlayers(self, x):
+        self.__players = x
+
 
 if __name__ == "__main__":
     pass
