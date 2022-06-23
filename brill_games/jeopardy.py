2108dcbf42742b29abacd57241d07088345f20f4
chrisphamm521 <chris.pham2003@rocketmail.com>
2022-06-22 21:24:30 -0400

    Added class Answer to jeopardy.py with constructor, getters , and setters

--- brill_games/jeopardy.py
+++ brill_games/jeopardy.py
@@ -1,4 +1,28 @@
 from tkinter import *
 
+class Answer:
+    def __init__(self, answer, question, points):
+        self.__answer = answer
+        self.__question = question
+        self.__points = points
+
+    def getAnswer(self):
+        return self.__answer
+
+    def getQuestion(self):
+        return self.__question
+
+    def getPoints(self):
+        return self.__points
+
+    def setAnswer(self, x):
+        self.__answer = x
+
+    def setQuestion(self, y):
+        self.__question = y
+
+    def setPoints(self, z):
+        self.__points = z
+
 if __name__ == "__main__":
     pass
